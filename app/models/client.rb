7f0a277976b901e5fdbabcd14d85e22fc54b7bf2
ameerh90 <ameer.haider@tkxel.com>
2013-12-21 16:31:49 +0500

    Up to date

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  attr_accessible :company_name, :contact_person, :description, :email, :phone_number, :slogan, :title, :website, :city, :state, :country, :street_number, :address, :logo, :latitude, :longitude
+  attr_accessible :company_name, :contact_person, :description, :email, :phone_number, :slogan, :title, :website, :city, :state, :country, :address1, :address2, :logo, :latitude, :longitude
   mount_uploader :logo, LogoUploader
 
   extend FriendlyId
